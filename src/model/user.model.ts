2b57dfcdd649eb6a4d7ac1418b8cc71c27bae15c
Mac <lawblaze4@gmail.com>
2024-12-27 00:08:52 +0100

    boiler plate code

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,27 +1,11 @@
 import { Schema, model } from "mongoose";
 
-enum UserType {
-    Admin = "admin",
-    User = "user"
-}
-
 const schema = new Schema(
     { 
         username: { type: String, required: false },
         email: { type: String, required: true },
         password: { type: String, required: true },
-        image: { type: String, required: true, default: "https://img.icons8.com/material-rounded/100/user.png" },
-        isVerified: { type: Boolean, required: true, default: false },
-        role: { type: String, enum: Object.values(UserType), required: true, default: UserType.User },
-        state:{type: String, required: false},
-        lga:{type: String, required: false},
-        city:{type: String, required:false},
-        contact:{type: String,required:false},
-        address:{type: String, required:false},
-        link:{type: String, required:false},
-        name:{type: String, required:false},
-        phone:{type: String, required:false},
-        whatsapp:{type:String, required:false}
+      
     },
     { timestamps: true } // Adds createdAt and updatedAt fields
 );
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,37 +1,7 @@
 import express from 'express';
-import authClass from '../controller/auth/auth.class';
-import adminClass from '../controller/admin.class';
-import isAuthenticated from '../middleware/authorization';
-import userClass from '../controller/user.class';
+
 
 const router = express.Router();
-//auth routes
-router.post('/auth/login',authClass.login)
-router.post('/auth/register',authClass.register)
-router.post('/auth/forget-password',authClass.register)
-router.get('/auth/user',isAuthenticated,authClass.getAuthUser)
-router.patch('/auth/user',isAuthenticated,authClass.update)
-router.patch('/auth/user/uploadProfile',isAuthenticated,authClass.updateProfile)
-//dashboard routes admin
-router.get('/dashboard',isAuthenticated,adminClass.getDashboardDetails)
-router.get('/products',isAuthenticated,adminClass.getProducts)
-router.patch('/products',isAuthenticated,adminClass.acceptDeclineProduct)
-//user routes
-router.get('/user',isAuthenticated,adminClass.getAllUsers)
-//dashboard details user
-router.get('/promoDetails',isAuthenticated,userClass.getProduct)
-router.get('/promoDetailsCount',isAuthenticated,userClass.getProductCount)
-router.get('/promoAdminDetailsCount',isAuthenticated,userClass.getAdminProductCount)
-router.post('/promo',isAuthenticated,userClass.uploadPromo)
-router.patch('/promoTemp',isAuthenticated,userClass.updateTemprature)
-//public routes
-router.get('/promo',userClass.getPromo)
-//notification routes
-router.get('/notifications',isAuthenticated,userClass.getNotification)
-
-//banner images
-router.post('/bannerImages',isAuthenticated,adminClass.uploadBannerImage)
-router.get('/bannerImages',adminClass.bannerImage)
 
 
 
